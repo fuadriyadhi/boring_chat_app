e4158d3a192ad576f776d37ca31c2c543eaff438
fuadriyadhi <fuadriyadhi678@gmail.com>
2022-12-20 14:45:08 +0700

    final-up

--- src/firebaseServices.js
+++ src/firebaseServices.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB3H3xaRd67ojqvY9jtAVDLB1xxT43WmZM",
-  authDomain: "boring-chat-app-8a194.firebaseapp.com",
-  projectId: "boring-chat-app-8a194",
-  storageBucket: "boring-chat-app-8a194.appspot.com",
-  messagingSenderId: "839068027215",
-  appId: "1:839068027215:web:dac07d31a1aaa4e398dd3a",
+  apiKey: import.meta.env.VITE_FB_APIKEY,
+  authDomain: import.meta.env.VITE_FB_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_FB_PROJECTID,
+  storageBucket: import.meta.env.VITE_FB_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_FB_MESSENDER_ID,
+  appId: import.meta.env.VITE_FB_APPID,
 };
 
 // Initialize Firebase
